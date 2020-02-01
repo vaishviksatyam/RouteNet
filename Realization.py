423d55008ba1f201ef8b2412a07725b4ca9238a6
Vaishvik <vsatyam28@gmail.com>
2020-02-02 01:00:39 +0530

    Removed Global Variable File, Added definitions and sudo actions to be performed by the robot

--- Realization.py
+++ Realization.py
@@ -2,7 +2,7 @@
 
 #Weather defines how often robot shivers
 #Freshness defines how fast robot responds to voice
-#Life defines breaking in Speed
+#Life defines breaking in Speed and with how much stiffness he walks with
 #Exploration how often robot gets distracted
 #Cheerful how often he jumps
 #Active How fast he walks and turns
@@ -50,4 +50,24 @@ def Current_Behaviour(): # Fetch data from Table
 	print("Cheerful:",Cheerful)
 	print("Active:",Active)
 
-Current_Behaviour()
+def Walk(Life,Active,cheerful_delay):
+	pass
+	#Define id,Type,Complience,Speed,Delay for motors and its walk cycle
+def Standby(Weather_delay,Life,Exploration_delay):
+	pass
+	#How often it will react to weather, Exploration and how stiff it will be.
+def Voice(Freshness):
+	pass
+	#Reaction time
+def going_to_sleep(Life,Active):
+	pass
+	#How stiff is he and how fast
+def waking_up(Life,Active):
+	pass
+	#How stiff he is and how fast
+def Explore(Life,Weather,Active,Exploration_delay):
+	pass
+	#Random Walking with stiffness and speed,and usualy Head Movement.
+def Play_time(Active):
+	pass
+	#How frequenctly it changes its action
